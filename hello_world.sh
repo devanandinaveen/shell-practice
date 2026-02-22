2b15d43295d89f5bb7b2bc41de943980986269b5
Naveen <devanandinaveen@gmail.com>
2026-02-22 12:28:44 +0800

    Convo

--- hello_world.sh
+++ hello_world.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "hellow world"
+echo "suresh :: Hello Reamesh, how are you?"
+echo "Ramesh :: Hi Suresh, im good . how are you"
+echo "Suresh :: im good too. what are you learning?"
+echo "Ramesh :: Im learning Shell. what about you?"
