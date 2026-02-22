02283417167f4ba9331515fcfa7636f358c51f48
Naveen <devanandinaveen@gmail.com>
2026-02-22 14:01:36 +0800

    dynamic variable added

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :: Hello $PERSON2, how are you?"
+echo "$PERSON2 :: Hi $PERSON1, im good . how are you"
+echo "$PERSON1 :: im good too. what are you learning?"
+echo "$PERSON2 :: Im learning Shell. what about you?"
