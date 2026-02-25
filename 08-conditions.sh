9c14803d64a88cff451a3facef609ea75f679c70
Naveen <devanandinaveen@gmail.com>
2026-02-25 16:45:14 +0800

    conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,10 +2,8 @@
 
 Number=$1
 
-if [ $Number -gt 20 ]; 
- then 
-echo "Given number : $Number is greater than 20"
-
+if [ $Number -gt 20 ]; then 
+  echo "Given number : $Number is greater than 20"
 else 
- echo "Given Number :$Number is less than 20"
+  echo "Given Number :$Number is less than 20"
  fi
